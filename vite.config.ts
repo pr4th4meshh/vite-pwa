eaa8a0aa54733f377c2f5562278f78372a760440
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2023-11-20 15:17:59 +0530

    ready pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: "prompt",
@@ -17,35 +17,74 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
         src: "maskable_icon.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable any"
+        purpose: "maskable any",
       },
       {
         src: "logo192.png",
         sizes: "192x192",
-        type: "image/png"
+        type: "image/png",
       },
       {
         src: "logo256.png",
         sizes: "256x256",
-        type: "image/png"
+        type: "image/png",
       },
       {
         src: "logo384.png",
         sizes: "384x384",
-        type: "image/png"
+        type: "image/png",
       },
       {
         src: "logo512.png",
         sizes: "512x512",
-        type: "image/png"
-      }
-    ]
-  }
+        type: "image/png",
+      },
+    ],
+  },
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  VitePWA()
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Wallet App",
+        short_name: "Wallet App",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "maskable_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "logo384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 })
